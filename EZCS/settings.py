34dc44f8ba4e8d2257b04982b119d5969c274cff
Jin <82833598+JinSeongjin@users.noreply.github.com>
2024-06-27 12:29:39 +0900

    Update settings.py (#8)

--- EZCS/settings.py
+++ EZCS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,9 +75,13 @@ WSGI_APPLICATION = "EZCS.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EZCS',
+        'USER': os.environ['MySQL_ID'],
+        'PASSWORD': os.environ['MySQL_PW'],
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
